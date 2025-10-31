fa27dfc896062967eacb7928836d3ec90c105e75
Michael Wang <michael.wang.physics@gmail.com>
2025-11-01 00:59:33 +0100

    initial setup

--- src/FunWithMIPS.jl
+++ src/FunWithMIPS.jl
@@ -1,5 +1,20 @@
 module FunWithMIPS
+    using StaticArrays
+    using DataStructures
+    using GLMakie
 
-# Write your package code here.
+    """
+    Flush output so that jobs can be monitored on a cluster.
+    """
+    @inline println(args...) = println(stdout, args...)
+    @inline function println(io::IO, args...)
+        Base.println(io, args...)
+        flush(io)
+    end
 
+    include("particles.jl")
+    include("neighbor_lists.jl")
+    include("interactions.jl")
+    include("integrators.jl")
+    include("system.jl")
 end
